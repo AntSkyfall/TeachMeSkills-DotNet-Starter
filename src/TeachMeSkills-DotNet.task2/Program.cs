7cebbfa6f7d187c2101e9b0aad605a14af460274
Anton dqdw <antskyfall@gmail.com>
2020-09-18 21:24:54 +0300

    feat: add solution add task 2

---
+++ src/TeachMeSkills-DotNet.task2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TeachMeSkills_DotNet.task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "\nмоя строка 1";
+            string str2 = "\tмоя строка 2";
+            string str3 = "\aмоя строка 3";
+
+            Console.WriteLine(str);
+            Console.WriteLine(str2);
+            Console.WriteLine(str3);
+        }
+    }
+}
