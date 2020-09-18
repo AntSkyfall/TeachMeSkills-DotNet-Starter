9cb4d89be298f8b63050ee56365136f3b8e47abb
Anton dqdw <antskyfall@gmail.com>
2020-09-18 21:49:53 +0300

    feat: add solution task3

---
+++ src/TeachMeSkills-DotNet.Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace TeachMeSkills_DotNet.Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal pi = 3.141592653M;
+            Console.WriteLine("Enter radius ");
+            decimal r = decimal.Parse(Console.ReadLine());
+            decimal result = pi * (r * r);
+            Console.WriteLine(result);
+        }
+    }
+}
