6787ceb8db962fabfd6dc072412a474b7632a419
AntSkyfall <70284961+AntSkyfall@users.noreply.github.com>
2020-09-22 00:59:23 +0300

    add: solution task 5

---
+++ TeachMeSkills-DotNet.Task 5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TeachMeSkills_DotNet.Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value1 = 2;
+            int value2 = 2;
+            Console.WriteLine("addition " + value1 + value2);
+            Console.WriteLine("subtraction " + (value1 - value2));
+            Console.WriteLine("multiplication " + value1 * value2);
+            Console.WriteLine("Division " + value1 / value2);
+            Console.WriteLine("the remainder " + value1 % value2);
+        }
+    }
+}
