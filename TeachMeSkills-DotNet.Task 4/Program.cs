a4ac301e60f07686c3f980b149c124e4a7af9b3d
AntSkyfall <70284961+AntSkyfall@users.noreply.github.com>
2020-09-22 00:27:55 +0300

    add: solution task 4

---
+++ TeachMeSkills-DotNet.Task 4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TeachMeSkills_DotNet.Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double V, S, R, h;
+            const double pi = 3.141592653;
+            Console.WriteLine("imput radius");
+            R = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("imput height");
+            h = double.Parse(Console.ReadLine());
+
+            V = pi * Math.Pow(R,2) * h;
+            Console.WriteLine("volume = " + V);
+
+
+            S = 2 * pi * R * (R + h);
+            Console.WriteLine("surface area = " + S);
+
+
+
+
+        }
+    }
+}
