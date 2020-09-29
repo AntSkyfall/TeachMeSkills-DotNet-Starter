33546e9a81cec35e73949479212da3e286bdb3f4
AntSkyfall <70284961+AntSkyfall@users.noreply.github.com>
2020-09-29 17:31:42 +0300

    init: add solution task 8

---
+++ src/TeachMeSkills-DotNet.Task 8/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace TeachMeSkills_DotNet.Task_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите одно слово из предложенных: погода; дождь; снег; солнце; слякоть; лёд; облачно; осадки; градус; гроза.");
+            string word = Console.ReadLine();
+            switch (word)
+            {
+                case "погода":
+                    Console.WriteLine("weather");
+                    break;
+                case "дождь":
+                    Console.WriteLine("rain");
+                    break;
+                case "снег":
+                    Console.WriteLine("snow");
+                    break;
+                case "солнце":
+                    Console.WriteLine("the sun");
+                    break;
+                case "слякоть":
+                    Console.WriteLine("slush");
+                    break;
+                case "лёд":
+                    Console.WriteLine("ice");
+                    break;
+                case "облачно":
+                    Console.WriteLine("cloudy");
+                    break;
+                case "осадки":
+                    Console.WriteLine("precipitation");
+                    break;
+                case "градус":
+                    Console.WriteLine("degrees");
+                    break;
+                case "гроза":
+                    Console.WriteLine("storm");
+                    break;
+                default:
+                    Console.WriteLine("такого слова в переводчике нет.");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
