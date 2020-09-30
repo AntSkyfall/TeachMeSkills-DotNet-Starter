47ad28566a7f4481924bd43588ce9ed2944fcb8a
AntSkyfall <70284961+AntSkyfall@users.noreply.github.com>
2020-09-30 12:53:43 +0300

    init: add solution task 9 if else

---
+++ src/TeachMeSkills-DotNet.Task 9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TeachMeSkills_DotNet.Task_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("imput value ");
+            int value = int.Parse(Console.ReadLine());
+            if (value % 2 == 0)
+            Console.WriteLine("even");
+            else
+                Console.WriteLine("odd");
+        }
+    }
+}
