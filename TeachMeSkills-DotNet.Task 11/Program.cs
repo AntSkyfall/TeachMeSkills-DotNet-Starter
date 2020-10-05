7e047cd7c59c60812e707721e17085f74e3571e2
AntSkyfall <70284961+AntSkyfall@users.noreply.github.com>
2020-10-05 12:42:35 +0300

    init: add solution task 11

---
+++ TeachMeSkills-DotNet.Task 11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TeachMeSkills_DotNet.Task_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("imput value");
+            int value = int.Parse(Console.ReadLine());
+            if ((value &(value - 1)) ==0)
+                Console.WriteLine("The number is a power of two");
+            else
+                Console.WriteLine("Number is not a power of two ");
+        }
+    }
+}
