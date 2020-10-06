4f6900c8368448742b2d3156487b4ff9e0a581ae
AntSkyfall <70284961+AntSkyfall@users.noreply.github.com>
2020-10-06 22:19:47 +0300

    init: add solution task 16

---
+++ src/TeachMeSkills-DotNet.Task 16/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TeachMeSkills_DotNet.Task_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter height rectangle");
+            int height = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter width rectangle");
+            int width = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int a = 0; a < width; a++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("*");
+            }
+        }
+    }
+}
