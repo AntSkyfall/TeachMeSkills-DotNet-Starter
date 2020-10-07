48fa6dad1744367bd7636fff02d1f529ea6b2015
AntSkyfall <70284961+AntSkyfall@users.noreply.github.com>
2020-10-07 12:36:18 +0300

    init: add solution task 17

---
+++ src/TeachMeSkills-DotNet.Task 17/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TeachMeSkills_DotNet.Task_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the amount in Russian rubles ");
+            double rusRub = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the rate to convert");
+            double rateConvert = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Conversion result: { rateConvert * 100/rusRub }" );
+        }
+    }
+}
